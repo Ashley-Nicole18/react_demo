2e9c50f7422aef64d04d68a44021fb2446306a60
Ashley-Nicole18 <ashleynicolegellado@gmail.com>
2025-04-07 19:08:08 +0800

    practice

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Greeting from "./components/Greeting";
+import ProductInfo from "./components/ProductInfo";
+
+
+
+const App = () => {
+
+  const numbers = [1,2,3,4,5];
+
+  return <main>
+    {numbers.map((number) => (
+      <ul key={number}>
+        <li>{number}</li>
+      </ul>
+    ))}
+  </main>
+
+};
+
+export default App;
